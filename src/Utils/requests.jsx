681eca9d162df930a4ff73021ef0ca840ec36c62
genetgig <153215188+genetgig@users.noreply.github.com>
2024-01-31 04:09:37 -0500

    favicon

--- src/Utils/requests.jsx
+++ src/Utils/requests.jsx
@@ -1,4 +1,4 @@
-const API_KEY ="c39c2bbb2ad01fb27701b96101fbe031";
+const API_KEY = import.meta.env.VITE_API_KEY;
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=214`,
